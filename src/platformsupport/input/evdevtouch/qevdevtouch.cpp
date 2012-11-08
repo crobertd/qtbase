db81129862b7783e7d8112d1ddbf025357287fa9
Robert Daniels <robert.daniels@vantagecontrols.com>
2012-11-08 12:05:35 -0700

    Fixed formatting issues for Change-Id: I4fc8ff404cad2083a57ff18737c5ea2b06d8ceac

--- src/platformsupport/input/evdevtouch/qevdevtouch.cpp
+++ src/platformsupport/input/evdevtouch/qevdevtouch.cpp
@@ -351,10 +351,10 @@ void QEvdevTouchScreenData::processInputEvent(input_event *data)
         }
 
     } else if (data->type == EV_KEY && !m_typeB) {
+
         if (data->code == BTN_TOUCH && data->value == 0)
-          {
             m_contacts[m_currentSlot].state = Qt::TouchPointReleased;
-          }
+
     } else if (data->type == EV_SYN && data->code == SYN_MT_REPORT && m_lastEventType != EV_SYN) {
 
         // If there is no tracking id, one will be generated later.
